4bd4acf5a536b530a2296aa51cf8df3d8a3f6722
Luca Mezzolla <lucamezzolla@gmail.com>
2022-05-02 19:17:13 +0200

    Modificata query lista pagamenti: aggiunto ordinamento per data discendente

--- pagamenti_be/src/main/java/cutalab/pagamenti/repositories/PaymentRepository.java
+++ pagamenti_be/src/main/java/cutalab/pagamenti/repositories/PaymentRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface PaymentRepository extends JpaRepository<PaymentEntity, Long> {
     
-    @Query(value="select * from payments where service_id = ? and payment_date between ? and ?", nativeQuery=true)
+    @Query(value="select * from payments where service_id = ? and payment_date between ? and ? order by payment_date desc", nativeQuery=true)
     List<PaymentEntity> list(Integer id, String from, String to);
     
 }
